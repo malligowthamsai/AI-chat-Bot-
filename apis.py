6ae4eb33e8bdeac0d11e8611fe9badbd0498ba13
malligowthamsai <malligowthamsai@gmail.com>
2026-04-06 23:13:54 +0530

    Add files via upload

---
+++ apis.py
@@ -0,0 +1,27 @@
+
+
+import requests
+
+API_KEY = "AIzaSyD0V1mLzYUt5Myb_vCGPyNJbZqOgvuO7Is"
+
+URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent"
+
+def ask_ai(prompt):
+    data = {
+        "contents": [
+            {
+                "parts": [
+                    {"text": prompt}
+                ]
+            }
+        ]
+    }
+
+    try:
+        response = requests.post(f"{URL}?key={API_KEY}", json=data)
+        result = response.json()
+
+        return result["candidates"][0]["content"]["parts"][0]["text"]
+
+    except Exception as e:
+        return f"Error: {e}"
